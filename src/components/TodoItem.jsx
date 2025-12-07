c1bc27710a9f1cf1e41054ba472ced6cd9383305
Omolade Lekan <omoladelekan99@gmail.com>
2025-12-07 13:07:34 +0100

    started work on implementing drag and drop

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import DeleteIcon from "./Delete";
 import TodoTask from "./TodoTask";
 
@@ -7,12 +8,24 @@ export default function TodoItem({
   handleTaskStatus,
   handleDeleteItem,
 }) {
+  const [draggedId, setDraggedId] = useState(null);
+
+  function handleDragItem(e) {
+    const draggedElement = e.target;
+
+    setDraggedId(todo.id);
+
+    e.dataTransfer.setDragImage(draggedElement, 0, 0);
+  }
+
   return (
     <li
       key={todo.id}
       className={`todo-item flex items-center justofy-stretch ${
         todo.id === selected && "swipe"
-      }`}
+      } ${draggedId && "dragged"}`}
+      draggable="true"
+      onDragStart={handleDragItem}
     >
       <TodoTask todo={todo} handleTaskStatus={handleTaskStatus} />
 
